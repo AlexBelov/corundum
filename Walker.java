8e1125717a90ac103f50135d13158c65304bdf37
Alexander Belov <git@belov.by>
2014-05-26 18:51:30 +0300

    walker style corrections

--- Walker.java
+++ Walker.java
@@ -9,71 +9,61 @@ import java.io.InputStream;
 import java.util.Stack;
 
 public class Walker {
-   public static class Evaluator extends CorundumBaseListener {
-    ParseTreeProperty<Integer> values = new ParseTreeProperty<Integer>();
-    //Integer need_pop = 1;
+    public static class Evaluator extends CorundumBaseListener {
+        ParseTreeProperty<Integer> values = new ParseTreeProperty<Integer>();
 
-    public void enterInt_assignment(CorundumParser.Int_assignmentContext ctx) {
-        System.out.println(".local int " + ctx.var_id.getText());
-    }
+        public void enterInt_assignment(CorundumParser.Int_assignmentContext ctx) {
+            System.out.println(".local int " + ctx.var_id.getText());
+        }
 
-    public void exitInt_assignment(CorundumParser.Int_assignmentContext ctx) {
-        String str = ctx.var_id.getText() + " " + ctx.op.getText() + " " + values.get(ctx.getChild(2));
-        System.out.println(str);
-    }
+        public void exitInt_assignment(CorundumParser.Int_assignmentContext ctx) {
+            String str = ctx.var_id.getText() + " " + ctx.op.getText() + " " + values.get(ctx.getChild(2));
+            System.out.println(str);
+        }
 
-    // public void exitRvalue(CorundumParser.RvalueContext ctx) {
-    //     if (need_pop == 1) {
-    //        System.out.println(stack.pop()); 
-    //        need_pop = 0;
-    //     }
-    // }
+        public void exitInt_result(CorundumParser.Int_resultContext ctx) {
+            if ( ctx.getChildCount() == 3 && ctx.op != null ) { // operation node
 
-    public void exitInt_result(CorundumParser.Int_resultContext ctx) {
-        if ( ctx.getChildCount() == 3 && ctx.op != null ) { // operation node
+                int left = values.get(ctx.getChild(0));
+                int right = values.get(ctx.getChild(2));
 
-            int left = values.get(ctx.getChild(0));
-            int right = values.get(ctx.getChild(2));
-
-            switch(ctx.op.getType()) {
-                case CorundumParser.MUL:
-                    values.put(ctx, left * right);
-                    break;
-                case CorundumParser.DIV:
-                    values.put(ctx, left / right);
-                    break;
-                case CorundumParser.MOD:
-                    values.put(ctx, left % right);
-                    break;
-                case CorundumParser.PLUS:
-                    values.put(ctx, left + right);
-                    break;
-                case CorundumParser.MINUS:
-                    values.put(ctx, left - right);
-                    break;
+                switch(ctx.op.getType()) {
+                    case CorundumParser.MUL:
+                        values.put(ctx, left * right);
+                        break;
+                    case CorundumParser.DIV:
+                        values.put(ctx, left / right);
+                        break;
+                    case CorundumParser.MOD:
+                        values.put(ctx, left % right);
+                        break;
+                    case CorundumParser.PLUS:
+                        values.put(ctx, left + right);
+                        break;
+                    case CorundumParser.MINUS:
+                        values.put(ctx, left - right);
+                        break;
+                }
+            }
+            else if ( ctx.getChildCount() == 1 ) { // near-terminal node
+                values.put(ctx, values.get(ctx.getChild(0)));
+            }
+            else if ( ctx.getChildCount() == 3 && ctx.op == null ) { // node with brackets
+                values.put(ctx, values.get(ctx.getChild(1)));
             }
         }
-        else if ( ctx.getChildCount() == 1 ) {
+
+        public void exitInt_t(CorundumParser.Int_tContext ctx) {
             values.put(ctx, values.get(ctx.getChild(0)));
         }
-        else if ( ctx.getChildCount() == 3 && ctx.op == null ) {
-            values.put(ctx, values.get(ctx.getChild(1)));
+
+        public void visitTerminal(TerminalNode node) {
+            Token symbol = node.getSymbol();
+            if ( symbol.getType()==CorundumParser.INT ) {
+                values.put(node, Integer.valueOf(symbol.getText()));
+            }
         }
     }
-
-    public void exitInt_t(CorundumParser.Int_tContext ctx) {
-        values.put(ctx, values.get(ctx.getChild(0)));
-    }
-
-    public void visitTerminal(TerminalNode node) {
-        Token symbol = node.getSymbol();
-        if ( symbol.getType()==CorundumParser.INT ) {
-            values.put(node, Integer.valueOf(symbol.getText()));
-        }
-    }
-
-
-   }
 
     public static void main(String[] args) throws Exception {
         String inputFile = null;
@@ -86,10 +76,8 @@ public class Walker {
         CorundumLexer lexer = new CorundumLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         CorundumParser parser = new CorundumParser(tokens);
-        parser.setBuildParseTree(true);      // tell ANTLR to build a parse tree
-        ParseTree tree = parser.prog(); // parse
-        // show tree in text form
-        //System.out.println(tree.toStringTree(parser));
+        parser.setBuildParseTree(true);
+        ParseTree tree = parser.prog();
 
         ParseTreeWalker walker = new ParseTreeWalker();
 
