0e7c6cad9d348f4754c776545d97b1f2b99362c9
Alexander Belov <git@belov.by>
2014-05-31 20:40:44 +0300

    move Walker compilation to cor.sh

--- cor.sh
+++ cor.sh
@@ -2,3 +2,4 @@
 java -jar /usr/local/lib/antlr-4.2.2-complete.jar Corundum.g4
 javac Corundum*.java
 java -classpath /usr/local/lib/antlr-4.2.2-complete.jar:. org.antlr.v4.runtime.misc.TestRig Corundum prog -gui $1
+javac Walker.java Corundum*.java
--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-javac Walker.java Corundum*.java
 java -classpath /usr/local/lib/antlr-4.2.2-complete.jar:. Walker $1
