e9b64792f4e6e3527e77de07238245b889793fe9
Alexander Belov <git@belov.by>
2014-06-02 01:04:56 +0300

    :shipit: lab4: add writing to file

--- lab4.rb
+++ lab4.rb
@@ -4,15 +4,17 @@ mas = read_file("lab4.txt")
 length = len(mas)
 
 for(i = 1; i < length - 1; i+=1)
-	for(j = 0; j < length - i; j+=1)
-		if(mas[j] > mas[j+1])
-			buf = mas[j]
-			mas[j] = mas[j+1]
-			mas[j+1] = buf
-		end
-	end
+  for(j = 0; j < length - i; j+=1)
+    if(mas[j] > mas[j+1])
+      buf = mas[j]
+      mas[j] = mas[j+1]
+      mas[j+1] = buf
+    end
+  end
 end
 
 for(i = 0; i < length; i+=1)
-	p mas[i]
+  p mas[i]
 end
+
+write_file("lab4_sorted.txt", mas)
