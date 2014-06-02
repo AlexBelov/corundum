c3a37ec5858dbc93ed37d2ec66911a7574e319f3
Alexander Belov <git@belov.by>
2014-06-02 18:37:36 +0300

    lab4: add array output

--- lab4.rb
+++ lab4.rb
@@ -1,4 +1,12 @@
 mas = []
 mas = read_file("lab4.txt")
 mas = sort mas
+# puts_mas mas
 write_file("lab4_sorted.txt", mas)
+
+def puts_mas mas
+  length = len(mas)
+  for(i = 0; i < length; i+=1)
+    p mas[i]
+  end
+end
