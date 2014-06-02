08661bb5d1d27c66c5734aa9f314c265b7d5500c
Alexander Belov <git@belov.by>
2014-06-02 22:41:42 +0300

    edit scripts

--- test/lab4.rb
+++ test/lab4.rb
@@ -1,8 +1,8 @@
 mas = []
-mas = read_file "lab4.txt"
+mas = read_file "test/lab4.txt"
 mas = sort mas
 # puts_mas mas
-write_file "lab4_sorted.txt", mas
+write_file "test/lab4_sorted.txt", mas
 
 def puts_mas mas
   length = len(mas)
